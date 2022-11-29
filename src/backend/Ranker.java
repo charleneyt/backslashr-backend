7847e7689cad7ecb2225b9ef4aa2692b5a309085
ariel-zhao <ariel.zhao@certik.org>
2022-11-28 22:50:40 -0800

    refactor and add Ranker class

--- src/backend/Ranker.java
+++ src/backend/Ranker.java
@@ -89,38 +89,38 @@ public class Ranker {
 			}
 		}
 
-//					System.out.println("idfArray is: " + Arrays.toString(idfArray));
-//					for (String url : urlToFrequencies.keySet()) {
-//						System.out.println("~~~freqs for url " + url + " is: " + Arrays.toString(urlToFrequencies.get(url)));
-//					}
+//		System.out.println("idfArray is: " + Arrays.toString(idfArray));
+//		for (String url : urlToFrequencies.keySet()) {
+//			System.out.println("~~~freqs for url " + url + " is: " + Arrays.toString(urlToFrequencies.get(url)));
+//		}
 		
 		Map<Double, String> finalScores = new TreeMap<>(Collections.reverseOrder());
 		for (Map.Entry<String, int[]> entry : urlToFrequencies.entrySet()) {
 			String url = entry.getKey();
 			int[] freqs = entry.getValue();
-//						System.out.println("freqs for url " + url + " is: " + Arrays.toString(freqs));
+//			System.out.println("freqs for url " + url + " is: " + Arrays.toString(freqs));
 			double cosineScore = 0.0;
 			for (int i = 0; i < searchTerms.length; i++) {
 				cosineScore += freqs[i] * idfArray[i];
 			}
 			int wordCount = urlToWordCount.get(url);
 			cosineScore /= wordCount;
-//						System.out.println("cosine score for url " + url + " is: " + cosineScore);
+//			System.out.println("cosine score for url " + url + " is: " + cosineScore);
 			
 			// compute the final scores by multiplying cosine scores and page ranks
 			Row row = kvs.getRow("pageranks", url);
 			if (row != null) {
 				double pageRank = Double.valueOf(row.get("rank"));
-//							System.out.println("page rank for url " + url + " is: " + pageRank);
+//				System.out.println("page rank for url " + url + " is: " + pageRank);
 				double finalScore = cosineScore * pageRank * 1000;
-//							System.out.println("final score for url " + url + " is: " + finalScore);
+//				System.out.println("final score for url " + url + " is: " + finalScore);
 				finalScores.put(finalScore, url);
 			}
 		}
 		
 		int K = 100; // number of search results to display on front end
 		List<String> outputURLs = new ArrayList<>();
-		Iterator<Entry<Double, String>> iterator = finalScores.entrySet().iterator();	  	    
+		Iterator<Entry<Double, String>> iterator = finalScores.entrySet().iterator();
 		for (int i = 0; i < K; i++) {
 			if (iterator.hasNext()) {
 				outputURLs.add(iterator.next().getValue());
