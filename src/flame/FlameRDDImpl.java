98a8c4caec87def9d302724d44b40d74c23ad47e
cindy8tao <64484253+cindy8tao@users.noreply.github.com>
2022-12-10 22:43:17 -0500

    Update with one round page rank

--- src/flame/FlameRDDImpl.java
+++ src/flame/FlameRDDImpl.java
@@ -8,13 +8,13 @@ import java.nio.*;
 import java.util.*;
 
 public class FlameRDDImpl implements FlameRDD {
-    FlameContextImpl context;
+	FlameContextImpl context;
 	public String tableName;
 	String[] results;
 	KVSClient kvs;
 	Vector<Partition> latestAssignment;
 
-	public FlameRDDImpl(FlameContextImpl context){
+	public FlameRDDImpl(FlameContextImpl context) {
 		this.context = context;
 		tableName = null;
 		results = null;
@@ -22,16 +22,16 @@ public class FlameRDDImpl implements FlameRDD {
 		latestAssignment = context.latestAssignment;
 	}
 
-	public void saveTable(String tableName){
+	public void saveTable(String tableName) {
 		this.tableName = tableName;
 	}
 
-	public void saveResults(String[] results){
+	public void saveResults(String[] results) {
 		this.results = results;
 	}
 
 	@Override
-	public String getTableName(){
+	public String getTableName() {
 		return tableName;
 	}
 
@@ -39,21 +39,22 @@ public class FlameRDDImpl implements FlameRDD {
 	public List<String> collect() throws Exception {
 		List<String> ret = new ArrayList<>();
 		Iterator<Row> iter = kvs.scan(tableName, null, null);
-			// int count = kvs.count(tableName);
-			// System.out.println(count);
-			while (iter.hasNext()){
-				Row row = iter.next();
-				if (row == null){
-					break;
-				}
-				ret.add(row.get("value"));
+		// int count = kvs.count(tableName);
+		// System.out.println(count);
+		while (iter.hasNext()) {
+			Row row = iter.next();
+			if (row == null) {
+				break;
+			}
+			ret.add(row.get("value"));
 		}
 		return ret;
 	}
 
 	@Override
 	public FlameRDD flatMap(StringToIterable lambda) throws Exception {
-		String outputTable = context.invokeOperation(tableName, "/rdd/flatMap", Serializer.objectToByteArray(lambda), null, null);
+		String outputTable = context.invokeOperation(tableName, "/rdd/flatMap", Serializer.objectToByteArray(lambda),
+				null, null);
 		FlameRDDImpl ret = new FlameRDDImpl(context);
 		ret.saveTable(outputTable);
 
@@ -62,7 +63,8 @@ public class FlameRDDImpl implements FlameRDD {
 
 	@Override
 	public FlamePairRDD mapToPair(StringToPair lambda) throws Exception {
-		String outputTable = context.invokeOperation(tableName, "/rdd/mapToPair", Serializer.objectToByteArray(lambda), null, null);
+		String outputTable = context.invokeOperation(tableName, "/rdd/mapToPair", Serializer.objectToByteArray(lambda),
+				null, null);
 		FlamePairRDDImpl ret = new FlamePairRDDImpl(context);
 		ret.saveTable(outputTable);
 		return ret;
@@ -83,15 +85,16 @@ public class FlameRDDImpl implements FlameRDD {
 		ByteBuffer.wrap(bytes).putDouble(f);
 
 		String outputTable = context.invokeOperation(tableName, "/rdd/sample", bytes, null, null);
-		
+
 		FlameRDDImpl ret = new FlameRDDImpl(context);
 		ret.saveTable(outputTable);
 		return ret;
 	}
 
 	public FlamePairRDD groupBy(StringToString lambda) throws Exception {
-		String outputTable = context.invokeOperation(tableName, "/rdd/groupBy", Serializer.objectToByteArray(lambda), null, null);
-		
+		String outputTable = context.invokeOperation(tableName, "/rdd/groupBy", Serializer.objectToByteArray(lambda),
+				null, null);
+
 		FlamePairRDDImpl ret = new FlamePairRDDImpl(context);
 		ret.saveTable(outputTable);
 		return ret;
@@ -121,95 +124,110 @@ public class FlameRDDImpl implements FlameRDD {
 	public Vector<String> take(int num) throws Exception {
 		Vector<String> ret = new Vector<>();
 		num = Math.min(num, count());
-		while (num > 0){
+		while (num > 0) {
 			Iterator<Row> iter = kvs.scan(tableName, null, null);
-				while (num > 0 && iter.hasNext()){
-					Row row = iter.next();
-					if (row == null){
-						break;
-					}
-					ret.add(row.get("value"));
-					num--;
+			while (num > 0 && iter.hasNext()) {
+				Row row = iter.next();
+				if (row == null) {
+					break;
 				}
+				ret.add(row.get("value"));
+				num--;
+			}
 		}
 		return ret;
 	}
 
 	@Override
 	public String fold(String zeroElement, TwoStringsToString lambda) throws Exception {
-		String outputTable = context.invokeOperation(tableName, "/rdd/fold", Serializer.objectToByteArray(lambda), zeroElement, null);
+		String outputTable = context.invokeOperation(tableName, "/rdd/fold", Serializer.objectToByteArray(lambda),
+				zeroElement, null);
+
+//		Iterator<Row> rows = FlameContext.getKVS().scan(outputTable, null, null);
+//		System.out.println("TABLE NAME = " + outputTable);
+//		String sum = zeroElement;
+//		while (rows.hasNext()) {
+//			Row row = rows.next();
+//			String accumulator = row.get("Accumulator");
+//			sum = lambda.op(sum, accumulator);
+//			System.out.println("ROW = " + row + " SUM = " + sum);
+//		}
+//		return sum;
 
 		String accumulator = null;
 
-		for (Partition par : latestAssignment){
+		for (Partition par : latestAssignment) {
 			String distinguisher = par.fromKey != null ? par.fromKey : "null";
 			distinguisher += par.toKeyExclusive != null ? par.toKeyExclusive : "null";
-			int count = 0;
-			while (!kvs.existsRow(outputTable, distinguisher)){
-				if (count == 0){
-					System.out.println("waiting on distinguisher " + distinguisher);
-				}
-				count++;
-			}
-			accumulator = lambda.op(accumulator == null ? zeroElement : accumulator, new String(kvs.get(outputTable, distinguisher, "value")));
+//			int count = 0;
+//			while (!kvs.existsRow(outputTable, distinguisher)) {
+//				if (count == 0) {
+//					System.out.println("waiting on distinguisher " + distinguisher);
+//				}
+//				count++;
+//			}
+			accumulator = lambda.op(accumulator == null ? zeroElement : accumulator,
+					new String(kvs.get(outputTable, distinguisher, "value")));
 		}
 
 		return accumulator == null ? zeroElement : accumulator;
-		
 
-		// Iterator<Row> iter = kvs.scan(qParamsStrings[0], qParamsStrings[3], qParamsStrings[4]);
-		// if (iter != null) {
-		// 	while (iter.hasNext()) {
-		// 		Row row = iter.next();
-		// 		if (row == null) {
-		// 			break;
-		// 		}
-		// 		if (row.columns().contains(FlameWorker.VALUE_STRING)) {
-		// 			String s = row.get(FlameWorker.VALUE_STRING);
-		// 			if (s != null) {
-		// 				accumulator = lambda.op(accumulator == null ? zeroElement : accumulator, s);
-		// 			}
-		// 		}
-		// 	}
-		// }
+//		Iterator<Row> iter = kvs.scan(qParamsStrings[0], qParamsStrings[3], qParamsStrings[4]);
+//		if (iter != null) {
+//			while (iter.hasNext()) {
+//				Row row = iter.next();
+//				if (row == null) {
+//					break;
+//				}
+//				if (row.columns().contains(FlameWorker.VALUE_STRING)) {
+//					String s = row.get(FlameWorker.VALUE_STRING);
+//					if (s != null) {
+//						accumulator = lambda.op(accumulator == null ? zeroElement : accumulator, s);
+//					}
+//				}
+//			}
+//		}
 
-		// Row row;
-
-		// if (kvs.existsRow(outputTable, "htotal") && (row = kvs.getRow(outputTable, "htotal")) != null){
-		// 	String accumulator = null;
-		// 	for (String colName : row.columns()){
-		// 		accumulator = lambda.op(accumulator == null ? zeroElement : accumulator, row.get(colName));
-		// 	}
-		// 	return accumulator == null ? zeroElement : accumulator;
-		// } else {
-		// 	return zeroElement;
-		// }
+//		Row row;
+//
+//		if (kvs.existsRow(outputTable, "htotal") && (row = kvs.getRow(outputTable, "htotal")) != null) {
+//			String accumulator = null;
+//			for (String colName : row.columns()) {
+//				accumulator = lambda.op(accumulator == null ? zeroElement : accumulator, row.get(colName));
+//			}
+//			return accumulator == null ? zeroElement : accumulator;
+//		} else {
+//			return zeroElement;
+//		}
 	}
 
 	@Override
 	public FlamePairRDD flatMapToPair(StringToPairIterable lambda) throws Exception {
-		String outputTable = context.invokeOperation(tableName, "/rdd/flatMapToPair", Serializer.objectToByteArray(lambda), null, null);
+		String outputTable = context.invokeOperation(tableName, "/rdd/flatMapToPair",
+				Serializer.objectToByteArray(lambda), null, null);
 		FlamePairRDDImpl ret = new FlamePairRDDImpl(context);
 		ret.saveTable(outputTable);
 
 		return ret;
 	}
 
-    @Override
-    public FlameRDD filter(StringToBoolean lambda) throws Exception {
-        String outputTable = context.invokeOperation(tableName, "/rdd/filter", Serializer.objectToByteArray(lambda), null, null);
+	@Override
+	public FlameRDD filter(StringToBoolean lambda) throws Exception {
+		String outputTable = context.invokeOperation(tableName, "/rdd/filter", Serializer.objectToByteArray(lambda),
+				null, null);
 		FlameRDDImpl ret = new FlameRDDImpl(context);
 		ret.saveTable(outputTable);
 
 		return ret;
-    }
+	}
 
-    @Override
-    public FlameRDD mapPartitions(IteratorToIterator lambda) throws Exception {
-        String outputTable = context.invokeOperation(tableName, "/rdd/mapPartitions", Serializer.objectToByteArray(lambda), null, null);
+	@Override
+	public FlameRDD mapPartitions(IteratorToIterator lambda) throws Exception {
+		String outputTable = context.invokeOperation(tableName, "/rdd/mapPartitions",
+				Serializer.objectToByteArray(lambda), null, null);
 		FlameRDDImpl ret = new FlameRDDImpl(context);
 		ret.saveTable(outputTable);
 
 		return ret;
-    }
+	}
 }
--- src/jobs/PageRank.java
+++ src/jobs/PageRank.java
@@ -3,200 +3,191 @@ package jobs;
 import java.util.*;
 
 import flame.*;
-import kvs.*;
-import tools.*;
+import tools.Hasher;
 
 public class PageRank {
-    static boolean inputThreshold = false;
+	static boolean inputThreshold = false;
 
-    public static void run(FlameContext ctx, String[] args) throws Exception {
-      // add check for args, should be 1 for convergence threshold, and 1 optional
-        if (args.length < 1 || args.length > 2){
-            ctx.output("We are expecting one argument t for the convergence threshold, and one optional argument for percentage threshold!");
-            return;
-        }
-        double convergence = Double.parseDouble(args[0]);
+	public static void run(FlameContext ctx, String[] args) throws Exception {
+		System.out.println("Executing pagerank ...");
+		// add check for args, should be 1 for convergence threshold, and 1 optional
+		if (args.length < 1 || args.length > 2) {
+			ctx.output(
+					"We are expecting one argument t for the convergence threshold, and one optional argument for percentage threshold!");
+			return;
+		}
+		double convergence = Double.parseDouble(args[0]);
 
-        // EC 2 enhanced convergence criterion
-        double thres = -1.0;
-        if (args.length == 2){
-            thres = Double.parseDouble(args[1]);
-            if (thres > 0 && thres < 100){
-                inputThreshold = true;
-            }
-        }
-        
-        if (FlameContext.getKVS() == null){
-            FlameContext.setKVS("localhost:8000");
-        }
-        KVSClient kvs = FlameContext.getKVS();
-        int totalPages = kvs.count("outdegrees");
+		// EC 2 enhanced convergence criterion
+		double thres = -1.0;
+		if (args.length == 2) {
+			thres = Double.parseDouble(args[1]);
+			if (thres > 0 && thres < 100) {
+				inputThreshold = true;
+			}
+		}
 
-        FlamePairRDD state = ctx.fromTable("outdegrees", r -> {
-                                        if (r.columns().contains("url") && r.get("url").length() > 0 && r.columns().contains("links") && r.get("links").length() >= 0){
-                                            try {
-                                                return r.key() + "|" + r.get("url") + ",1.0,1.0," + r.get("links");
-                                            } catch (Exception e) {
-                                                e.printStackTrace();
-                                                return null;
-                                            }
-                                        } else {return null;}
-                                    })
-                                    // then split the String into FlamePair
-                                    .mapToPair(s -> {
-                                        if (s != null && s.length() > 0){
-                                            int idx = s.indexOf("|");
-                                            return new FlamePair(s.substring(0, idx), s.substring(idx + 1));
-                                        } else {return null;}
-                                    });
-        int count = 0;
-        // Thread.sleep(totalPages*200);
-        // state.saveAsTable("state-" + count);
+		FlamePairRDD state = ctx.fromTable("outdegrees", r -> {
+			if (r.columns().contains("links") && r.get("links").length() >= 0) {
+				return r.key() + ",1.0,1.0," + r.get("links");
+			} else {
+				return null;
+			}
+		}).mapToPair(s -> {
+//			System.out.println("S = " + s);
+			if (s != null && s.length() > 0) {
+				int idx = s.indexOf(",");
+				return new FlamePair(s.substring(0, idx), s.substring(idx + 1));
+			} else {
+				return null;
+			}
+		});
+		int count = 0;
+		System.out.println("INITIAL STATE TABLE = " + state.getTableName() + "-" + count);
 
-        while (true){
-            count++;
-            FlamePairRDD transfer = state.flatMapToPair(pair -> {
-                                            List<FlamePair> ret = new ArrayList<FlamePair>();
-                                            if (!"".equals(pair._1())){
-                                                int firstComma = pair._2().indexOf(",");
-                                                int secondComma = pair._2().indexOf(",", firstComma+1);
-                                                int thirdComma = pair._2().indexOf(",", secondComma+1);
-                                                if (firstComma == -1 || secondComma == -1 || thirdComma == -1){
-                                                    return ret;
-                                                }
-                                                double currRank = Double.parseDouble(pair._2().substring(firstComma+1, secondComma));
-                                                if (thirdComma != pair._2().length() - 1){
-                                                    String urlList = pair._2().substring(thirdComma+1);
-                                                    String[] links = urlList.split(",");
-                                                    int n = links.length;
-                                                    if (n == 0){
-                                                        return ret;
-                                                    }
-                                                    String val = String.valueOf(0.85 * currRank / n);
-                                                    for (String link : links){
-                                                        String key = Hasher.hash(link);
-                                                        if ("".equals(key)){
-                                                            System.out.println("Hashed an empty link: "+link);
-                                                        } else {
-                                                            FlamePair generatedPair = new FlamePair(key, val);
-                                                            ret.add(generatedPair);
-                                                            // System.out.println(generatedPair);
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                            return ret;
-                                        });
-                // Thread.sleep(totalPages*200);
-                transfer = transfer.foldByKey("0.15", (d1, d2) -> String.valueOf(Double.parseDouble(d1) + Double.parseDouble(d2)));
-            System.out.println("transfer table: "+transfer.getTableName());
-                // Thread.sleep(totalPages*200);
-            // Thread.sleep(kvs.count(transfer.getTableName())/10);
-            // transfer.saveAsTable("transfer-" + count);
-//            Thread.sleep(kvs.count("transfer-" + count)/10);
+//		while (true) {
+		count++;
+		FlamePairRDD transfer = state.flatMapToPair(pair -> {
+//				System.out.println("TRANSFER = " + pair._1() + " ===> " + pair._2());
+			List<FlamePair> ret = new ArrayList<FlamePair>();
+			if (!"".equals(pair._1())) {
+				int firstComma = pair._2().indexOf(",");
+				int secondComma = pair._2().indexOf(",", firstComma + 1);
+				if (firstComma == -1 || secondComma == -1) {
+					return ret;
+				}
+				double currRank = Double.parseDouble(pair._2().substring(0, firstComma));
+				if (secondComma != pair._2().length() - 1) {
+					String urlList = pair._2().substring(secondComma + 1);
+					String[] links = urlList.split(",");
+					int n = links.length;
+					if (n == 0) {
+						return ret;
+					}
+					String val = String.valueOf(0.85 * currRank / n);
+					for (String link : links) {
+						String hashLink = Hasher.hash(link);
+						FlamePair generatedPair = new FlamePair(hashLink, val);
+						ret.add(generatedPair);
+					}
+				}
+			}
+			return ret;
+		});
+		System.out.println("TRANSFER TABLE = " + transfer.getTableName() + "-" + count);
+		transfer = transfer.foldByKey("0.15",
+				(d1, d2) -> String.valueOf(Double.parseDouble(d1) + Double.parseDouble(d2)));
 
-            state = transfer.join(state);
-            System.out.println("joined table: "+state.getTableName());
-            // Thread.sleep(totalPages*200);
-            state = state.flatMapToPair(pair -> {
-                                List<FlamePair> ret = new ArrayList<>();
-                                if (!"".equals(pair._1())){
-                                    String str = pair._2();
-                                    int firstComma = str.indexOf(",");
-                                    int secondComma = str.indexOf(",", firstComma+1);
-                                    int thirdComma = str.indexOf(",", secondComma+1);
-                                    int fourthComma = str.indexOf(",", thirdComma+1);
-                                    if (firstComma == -1 || secondComma == -1 || thirdComma == -1 || fourthComma == -1){
-                                        return ret;
-                                    }
-                                    String newVal = str.substring(firstComma+1, secondComma) +","+ str.substring(0, firstComma) + str.substring(secondComma, thirdComma) + str.substring(fourthComma);
-                                    ret.add(new FlamePair(pair._1(), newVal));
-                                }
-                                return ret;
-                            });
-            System.out.println("next round state: "+state.getTableName());
-            // Thread.sleep(totalPages*200);
-            // Thread.sleep(kvs.count(state.getTableName())/10);
-            // state.saveAsTable("state-" + count);
-//            Thread.sleep(kvs.count("state-" + count)/10);
-            
-            FlameRDD diffTable = state.flatMap(pair -> {
-                                List<String> ret = new ArrayList<>();
-                                if (!"".equals(pair._1())){
-                                    String str = pair._2();
-                                    int firstComma = str.indexOf(",");
-                                    int secondComma = str.indexOf(",", firstComma+1);
-                                    int thirdComma = pair._2().indexOf(",", secondComma+1);
-                                    if (firstComma == -1 || secondComma == -1 || thirdComma == -1){
-                                        return ret;
-                                    }
-                                    double currRank = Double.parseDouble(pair._2().substring(firstComma+1, secondComma));
-                                    double prevRank = Double.parseDouble(pair._2().substring(secondComma+1, thirdComma));
-                                    ret.add(str.substring(0, firstComma+1) + String.valueOf(Math.abs(currRank - prevRank)));
-                                }
-                                return ret;
-                            });
-            // Thread.sleep(totalPages*200);
-            // diffTable.saveAsTable("diff-" + count);
+		System.out.println("TRANSFER AFTER FOLD TABLE = " + transfer.getTableName() + "-" + count);
+//			System.out.println("STATE TABLE BEFORE JOIN  = " + state.getTableName() + "-" + count);
 
-            // EC 2 enhanced convergence: converge sooner if percentage is satisfied!
-            boolean converged = false;
-            if (inputThreshold){
-                List<String> diffList = diffTable.collect();
-                int needed = (int) (diffList.size() * thres / 100 + 0.5);
-                for (String diff : diffList){
-                	int firstComma = diff.indexOf(",");
-                	if (firstComma != -1) {
-                		if (Double.parseDouble(diff.substring(firstComma+1)) <= convergence){
-	                      needed--;
-	                  }
-                	}
-                  if (needed == 0){
-                    System.out.println("Checked with enhanced convergence!");
-                    converged = true;
-                    break;
-                  }
-                }
-            } else {
-                String maxDiffStr = diffTable.fold("0.0", (s1, s2) -> {
-                                double s1Double = Double.parseDouble(s1.substring(s1.indexOf(",")+1));
-                                double s2Double = Double.parseDouble(s2.substring(s2.indexOf(",")+1));
-                                if (s1Double < s2Double){
-                                    return String.valueOf(s2Double);
-                                } else {
-                                    return String.valueOf(s1Double);
-                                }
-                            });
-                // Thread.sleep(totalPages*200);
-                System.out.println(maxDiffStr);
-                if (Double.parseDouble(maxDiffStr) < convergence){
-                    System.out.println("Checked with regular convergence!");
-                    break;
-                }
-            }
-            if (converged){
-                break;
-            }
-            System.out.println("Checked " + count + " rounds, still not converged");
-        }
-        System.out.println("Total iteration: " + count);
-        // state.saveAsTable("state");
-        state = state.flatMapToPair(pair -> {
-                List<FlamePair> ret = new ArrayList<>();
-                if (!"".equals(pair._1())){
-                    String str = pair._2();
-                    int firstComma = str.indexOf(",");
-                    int secondComma = str.indexOf(",", firstComma+1);
-                    if (firstComma == -1 || secondComma == -1){
-                        return ret;
-                    }
-                    ret.add(new FlamePair(pair._1(), str.substring(0, secondComma)));
-                }
-                return ret;
-              });
-        // Thread.sleep(totalPages*200);
-    //    Thread.sleep(kvs.count(state.getTableName())/10);
-       state.saveAsTable("pageranks");
-       ctx.output("OK");
-    }
+//			state = transfer.join(state);
+//			System.out.println("STATE TABLE AFTER JOIN = " + state.getTableName() + "-" + count);
+//
+//			state.flatMapToPair(pair -> {
+//				System.out.println("SHIFT = " + pair._1() + " ===> " + pair._2());
+//				List<FlamePair> ret = new ArrayList<>();
+//				if (!"".equals(pair._1())) {
+//					String str = pair._2();
+//					int firstComma = str.indexOf(",");
+//					int secondComma = str.indexOf(",", firstComma + 1);
+//					int thirdComma = str.indexOf(",", secondComma + 1);
+//					if (firstComma == -1 || secondComma == -1 || thirdComma == -1) {
+//						return ret;
+//					}
+//					String newVal = str.substring(0, secondComma) + str.substring(thirdComma);
+//					ret.add(new FlamePair(pair._1(), newVal));
+//				}
+//				return ret;
+//			});
+
+		state = transfer.join(state).flatMapToPair(pair -> {
+//				System.out.println("SHIFT = " + pair._1() + " ===> " + pair._2());
+			List<FlamePair> ret = new ArrayList<>();
+			if (!"".equals(pair._1())) {
+				String str = pair._2();
+				int firstComma = str.indexOf(",");
+				int secondComma = str.indexOf(",", firstComma + 1);
+				int thirdComma = str.indexOf(",", secondComma + 1);
+				if (firstComma == -1 || secondComma == -1 || thirdComma == -1) {
+					return ret;
+				}
+				String newVal = str.substring(0, secondComma) + str.substring(thirdComma);
+				ret.add(new FlamePair(pair._1(), newVal));
+			}
+			return ret;
+		});
+		System.out.println("STATE TABLE AFTER SHIFTING = " + state.getTableName() + "-" + count);
+
+		FlameRDD diffTable = state.flatMap(pair -> {
+//				System.out.println("DIFF = " + pair._1() + " ===> " + pair._2());
+			List<String> ret = new ArrayList<>();
+			if (!"".equals(pair._1())) {
+				String str = pair._2();
+				int firstComma = str.indexOf(",");
+				int secondComma = str.indexOf(",", firstComma + 1);
+				if (firstComma == -1 || secondComma == -1) {
+					return ret;
+				}
+				double currRank = Double.parseDouble(pair._2().substring(0, firstComma));
+				double prevRank = Double.parseDouble(pair._2().substring(firstComma + 1, secondComma));
+				ret.add(String.valueOf(Math.abs(currRank - prevRank)));
+			}
+			return ret;
+		});
+		System.out.println("DIFF TABLE = " + diffTable.getTableName() + "-" + count);
+
+		// EC 2 enhanced convergence: converge sooner if percentage is satisfied!
+		boolean converged = false;
+//			if (inputThreshold) {
+//				List<String> diffList = diffTable.collect();
+//				int needed = (int) (diffList.size() * thres / 100 + 0.5);
+//				for (String diff : diffList) {
+//					if (Double.parseDouble(diff) <= convergence) {
+//						needed--;
+//					}
+//					if (needed == 0) {
+//						System.out.println("Checked with enhanced convergence!");
+//						converged = true;
+//						break;
+//					}
+//				}
+//			} else {
+		String maxDiffStr = diffTable.fold("0.0", (s1, s2) -> {
+			double s1Double = Double.parseDouble(s1);
+			double s2Double = Double.parseDouble(s2);
+			if (s1Double < s2Double) {
+				return s2;
+			} else {
+				return s1;
+			}
+		});
+		System.out.println("Check max difference => " + maxDiffStr + " : " + convergence);
+		if (Double.parseDouble(maxDiffStr) < convergence) {
+			System.out.println("Checked with regular convergence!");
+//				break;
+		}
+//			}
+//			if (converged) {
+//				break;
+//			}
+		System.out.println("Round " + count + ", not converged");
+//		}
+		System.out.println("Total iteration: " + count);
+
+		state = state.flatMapToPair(pair -> {
+			List<FlamePair> ret = new ArrayList<>();
+			if (!"".equals(pair._1())) {
+				String str = pair._2();
+				int firstComma = str.indexOf(",");
+				if (firstComma == -1) {
+					return ret;
+				}
+				ret.add(new FlamePair(pair._1(), str.substring(0, firstComma)));
+			}
+			return ret;
+		});
+		state.saveAsTable("pageranks");
+		ctx.output("OK");
+	}
 }
